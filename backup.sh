2b8e43826b6c1a9952f7c43a487f18150bd471ec
Ashutosh Raghuwanshi <ashutosh@dynamicpdf.com>
2022-01-26 22:37:25 +0530

    Moved files to profile folder and added gitconfig.
    .gitconfig contains some private settings so public setting are moved to
    include file mygitconfig.inc for proper separation.

--- backup.sh
+++ backup.sh
@@ -3,21 +3,24 @@
 # Coppies any files from target folder to this folder (repository).
 # But only if they are already present in the repository.
 
-targetFolder=$HOME
+srcFolder=$HOME
+dstFolder=profile
 
 if [ -d "$1" ]; then
-	targetFolder=$1
+	srcFolder=$1
 fi
 
-for f in $(ls -A); do
-	if [ -f $f ]; then # Not a folder
-		if [ -f $targetFolder/$f ]; then # File is present in target folder.
-			cp $targetFolder/$f ./$f
-			echo "Coppied $targetFolder/$f"
+for f in $(ls -A $dstFolder); do # List files under profile folder.
+	if [ -f $dstFolder/$f ]; then # Not a folder. Subfolders will not be backed up.
+		if [ -f $srcFolder/$f ]; then # File is present in target folder.
+			cp $srcFolder/$f $dstFolder/$f
+			echo "Coppied $srcFolder/$f"
 		fi
 	fi
 done
 
+git diff
 # Update the repository. Commit can be done manually after making
 # sure that everything is alright.
 git add --update
+
